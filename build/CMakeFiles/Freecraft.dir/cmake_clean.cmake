817a61a9d61f41f13e2ff5e51c7024ebdee19178
Promesis <M3149154722@outlook.com>
2023-01-19 22:35:17 +0800

    Please enter the commit message for your changes. Lines starting  with '' will be ignored, and an empty message aborts the commit.
     On branch master
     Your branch is ahead of 'origin/master' by 1 commit.
       (use "git push" to publish your local commits)
    
     Changes to be committed:
    	modified:   CMakeLists.txt
    	new file:   build/.cmake/api/v1/reply/cache-v2-fee1c3d2fd7dabaf0828.json
    	new file:   build/.cmake/api/v1/reply/cmakeFiles-v1-4f0e6833f4bb5d6e1660.json
    	new file:   build/.cmake/api/v1/reply/codemodel-v2-973665a32608f3d081e1.json
    	new file:   build/.cmake/api/v1/reply/directory-.-Debug-d0094a50bb2071803777.json
    	new file:   build/.cmake/api/v1/reply/index-2023-01-19T14-31-56-0312.json
    	new file:   build/.cmake/api/v1/reply/target-Freecraft-Debug-4d8c0adff868c3b0685b.json
    	new file:   build/.cmake/api/v1/reply/toolchains-v1-754e0fc8575f63e75ef2.json
    	new file:   build/CMakeFiles/CMakeDirectoryInformation.cmake
    	new file:   build/CMakeFiles/Freecraft.dir/DependInfo.cmake
    	new file:   build/CMakeFiles/Freecraft.dir/build.make
    	new file:   build/CMakeFiles/Freecraft.dir/cmake_clean.cmake
    	new file:   build/CMakeFiles/Freecraft.dir/compiler_depend.make
    	new file:   build/CMakeFiles/Freecraft.dir/compiler_depend.ts
    	new file:   build/CMakeFiles/Freecraft.dir/depend.make
    	new file:   build/CMakeFiles/Freecraft.dir/flags.make
    	new file:   build/CMakeFiles/Freecraft.dir/link.txt
    	new file:   build/CMakeFiles/Freecraft.dir/linkLibs.rsp
    	new file:   build/CMakeFiles/Freecraft.dir/objects1
    	new file:   build/CMakeFiles/Freecraft.dir/progress.make
    	new file:   build/CMakeFiles/Makefile.cmake
    	new file:   build/CMakeFiles/Makefile2
    	new file:   build/CMakeFiles/TargetDirectories.txt
    	new file:   build/CMakeFiles/progress.marks
    	new file:   build/Makefile
    	new file:   build/cmake_install.cmake
    	new file:   build/compile_commands.json
        cmake built

---
+++ build/CMakeFiles/Freecraft.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Freecraft.dir/src/main.cpp.obj"
+  "CMakeFiles/Freecraft.dir/src/main.cpp.obj.d"
+  "Freecraft.exe"
+  "Freecraft.exe.manifest"
+  "Freecraft.pdb"
+  "libFreecraft.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Freecraft.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
