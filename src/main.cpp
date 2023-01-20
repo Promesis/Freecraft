882c06e4f3d30f8be5bc34abb7a7c8b7452b4036
Promesis <M3149154722@outlook.com>
2023-01-20 15:19:43 +0800

    Please enter the commit message for your changes. Lines starting  with '' will be ignored, and an empty message aborts the commit.
     On branch master
     Your branch is up to date with 'origin/master'.
    
     Changes to be committed:
    	modified:   CMakeLists.txt
    	new file:   include/glad/glad.h
    	new file:   include/glfw/glfw3.h
    	new file:   include/glfw/glfw3native.h
    	new file:   include/khr/khrplatform.h
    	new file:   src/glad/glad.c
    	modified:   src/main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,56 @@
+//test if glfw can be compiled.
+#include "glad/glad.h"
+#include "glfw/glfw3.h"
 
-#include <cstdlib>
+#include <iostream>
 
-int main(int argc,char **argv)
+void framebufferSizeCallback(GLFWwindow *, int, int);
+void inputCallback(GLFWwindow *);
+
+int main(int argc, char **argv)
 {
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 5);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    //glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+
+    GLFWwindow *window = glfwCreateWindow(800,600,"test",NULL,NULL);
+    if (window == NULL)
+    {
+        std::cout << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        std::cout << "Failed to initialize GLAD" << std::endl;
+        return -1;
+    }
+    glfwSetFramebufferSizeCallback(window, framebufferSizeCallback);
+    glViewport(0, 0, 800, 600);
+
+    while( !glfwWindowShouldClose(window) ) // basic render loop(actually i rendered nothing)
+    {
+        inputCallback(window);
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
     return EXIT_SUCCESS;
+}
+
+void framebufferSizeCallback(GLFWwindow *window, int width, int height)
+{
+    glViewport(0, 0, width, height);
+}
+
+void inputCallback(GLFWwindow *window)
+{
+    if( glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS )
+    {
+        glfwSetWindowShouldClose(window, true);
+    }
 }
